2a4a91a8405f31a12ab420bdb13f4d4b5971a246
Rohit Kumar Pandey <rohitpandey@Rohits-MacBook-Air.local>
2023-11-01 11:00:33 +0530

    fixed s3 issue

--- 20_pipeline/s3.tf
+++ 20_pipeline/s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "codepipeline_artifact" {
   bucket = "pipeline-artifact-terraform-learning"
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm     = "AES256"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
       }
     }
   }
